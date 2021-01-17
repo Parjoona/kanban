f1acb1dad1eb1c83afab7ce6fb3c596b419bd850
Par Joona <parjoona@hotmail.com>
2021-01-17 21:36:11 +0100

    fixed isActiveCheck for links

--- src/components/Navigation/NavigationButtons.tsx
+++ src/components/Navigation/NavigationButtons.tsx
@@ -3,24 +3,25 @@ import clsx from 'clsx';
 import { NavLink, useLocation } from 'react-router-dom';
 
 export default function NavigationButtons() {
-  const pages = [
+  const links = [
     { link: '/', name: 'Home' },
     { link: '/backlog', name: 'Backlog' },
     { link: '/board', name: 'Board' },
   ];
 
   const { pathname } = useLocation();
+  const isActive = (link: string) => pathname !== link;
 
   return (
     <div className="text-sm">
-      {pages.map(({ link, name }) => (
+      {links.map(({ link, name }) => (
         <NavLink to={link} activeClassName="bg-gray-700 text-blue-300">
           <div
             className={clsx(
               'p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300',
               {
-                'bg-gray-900 text-white': pathname !== link,
-                'bg-gray-700 text-blue-300': pathname === link,
+                'bg-gray-900 text-white': isActive(link),
+                'bg-gray-700 text-blue-300': !isActive(link),
               },
             )}
           >
